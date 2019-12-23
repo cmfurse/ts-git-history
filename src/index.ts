6fab4e24195d37481e3499bfe33920638a141d86
Clint Furse <cfurse@instructure.com>
2019-12-23 09:47:18 -0700

    added EJS and build tools

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 8080;
 
+// configure to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
 app.get("/", (request, response) => {
-    response.send("Hello World!");
+    response.render("index");
 });
 
 app.listen(PORT, () => {
